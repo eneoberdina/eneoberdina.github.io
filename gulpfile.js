47b21ea699aadde77cd7d0a8d3a82a28259abf96
Elena <eneoberdina@gmail.com>
2018-01-31 00:02:20 +0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
-const concat = require('gulp-concat-sourcemap');
 const sourcemaps = require('gulp-sourcemaps');
-const addsrc = require('gulp-add-src');
-const rename = require('gulp-rename');
 const notify = require('gulp-notify');
 const plumber = require('gulp-plumber');
 
 
 gulp.task('default', [
   
-    'build-css',
-	
+    'build-css',	
 ]);
 
 gulp.task('watch', () => {
@@ -26,15 +21,13 @@ gulp.task('build-css', () => {
         .pipe(plumber({
             errorHandler: notify.onError("Error: <%= error.message %>")
         }))
-        .pipe(sourcemaps.init())
+		.pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(autoprefixer({
             browsers: ['last 10 versions'],
         }))
-        .pipe(cleanCSS())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(sourcemaps.write('maps'))
-        .pipe(gulp.dest('css'))
+		.pipe(sourcemaps.write('maps'))
+		.pipe(gulp.dest('css'))
 });
 
 
