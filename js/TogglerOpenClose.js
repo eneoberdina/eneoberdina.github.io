7d16ebf2a0d399b7dbc73148bda91c0b52dcc776
Elena <eneoberdina@gmail.com>
2018-02-10 18:26:22 +0300

    fix

--- js/TogglerOpenClose.js
+++ js/TogglerOpenClose.js
@@ -1,93 +1,17 @@
 'use strict';
 
-let groups = { };
+function toggleOpen() {
+  document.getElementById("expand").classList.toggle("opened");
+  document.getElementById("expand").classList.toggle("closed");
 
-class Box {
-
-    constructor(node, from = 'closed', to = 'openned') {
-
-        this._node = node;
-        this._from = from;
-        this._to = to;
-
-    }
-
-    getNode() {
-
-        return this._node;
-
-    }
-
-    toggle() {
-
-        let node = this._node;
-
-        if (node.classList.contains(this._from)) {
-
-            this.setStateTo();
-
-        } else if (node.classList.contains(this._to)) {
-
-            this.setStateFrom();
-
-        } else {
-
-            this.setStateTo();
-
-        }
-
-    }
-
-    setStateFrom() {
-
-        let node = this._node;
-        node.classList.add(this._from);
-        node.classList.remove(this._to);
-
-    }
-
-    setStateTo() {
-
-        let node = this._node;
-        node.classList.add(this._to);
-        node.classList.remove(this._from);
-
-    }
-
+  document.getElementById("hidden-ul").classList.toggle("opened");
+  document.getElementById("ehidden-ul").classList.toggle("closed");
 }
 
+function toggleOpenSadebar() {
+  document.getElementById("hidd-block1").classList.toggle("opened");
+  document.getElementById("hidd-block1").classList.toggle("closed");
 
-function StateToggle(selector, group = null, state = 'closed => opened') {
-
-    let [from, to] = state.split(' => ');
-
-    let nodes = document.querySelectorAll(selector);
-    nodes = [].slice.call(nodes);
-
-    nodes.forEach(node => {
-
-        let box = new Box(node, from, to);
-        box.toggle();
-
-        if (!group) return;
-
-        if (!groups[group]) {
-
-            groups[group] = box;
-
-        } else if (groups[group].getNode() === node) {
-
-            groups[group] = null;
-
-        } else {
-
-            groups[group].setStateFrom();
-            groups[group] = box;
-
-        }
-
-    });
-
+  document.getElementById("active").classList.toggle("opened");
+  document.getElementById("active").classList.toggle("closed");
 }
-
-window.StateToggle = StateToggle;
